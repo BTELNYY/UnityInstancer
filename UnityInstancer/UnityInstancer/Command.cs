c4859937edf837205781b04b413fe51a0f5c793e
BTELNYY <btelnyy@gmail.com>
2025-01-05 22:49:46 -0600

    Not working yet

---
+++ UnityInstancer/UnityInstancer/Command.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnityInstancer
+{
+    public class Command
+    {
+        /// <summary>
+        /// The amount of params this command expects (note that the command invocation itself is not counted)
+        /// </summary>
+        public virtual int ParameterCount { get; } = 0;
+
+        /// <summary>
+        /// The proper syntax for the command
+        /// </summary>
+        public virtual string Syntax { get; } = "";
+
+        public virtual void Execute(string[] args) 
+        {
+
+        }
+    }
+}
